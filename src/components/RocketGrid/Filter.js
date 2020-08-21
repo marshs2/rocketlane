896d017d761660b42367189e5760cab890a3ac6f
Stephen Marshall <4u.marshall@gmail.com>
2020-08-22 03:45:36 +0530

    Format Filter.js

--- src/components/RocketGrid/Filter.js
+++ src/components/RocketGrid/Filter.js
@@ -1,38 +1,40 @@
-import React from 'react';
+import React from 'react'
 
-import './Filter.css';
+import './Filter.css'
 
 const Filter = (props) => {
-    return (
-        <React.Fragment>
-            {/*Show the filter data (hardcoded in API) in filter pane */}
-            {
-                props.headings.map((heading, i) => {
-                    return (
-                        <div key={i} className="filter-group">
-                            <div className="filter-heading">
-                                {heading[0].toUpperCase().concat(heading.slice(1))}
-                            </div>
-                            {Object.keys(props.filterMeta[heading]).map((list, i) => {
-                                return (
-                                    <div key={i} className="filter-block">
-                                        <div className="outer-label">
-                                            <label htmlFor={list}>{list}</label>
-                                        </div>
-                                        <input type="checkbox"
-                                            defaultChecked={props.filterMeta[heading][list]}
-                                            id={list} name={list} value={list}
-                                            onClick={props.filterChange}
-                                            data-group={heading}/>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    )
-                })
-            }
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      {/*Show the filter data (hardcoded in API) in filter pane */}
+      {props.headings.map((heading, i) => {
+        return (
+          <div key={i} className="filter-group">
+            <div className="filter-heading">
+              {heading[0].toUpperCase().concat(heading.slice(1))}
+            </div>
+            {Object.keys(props.filterMeta[heading]).map((list, i) => {
+              return (
+                <div key={i} className="filter-block">
+                  <div className="outer-label">
+                    <label htmlFor={list}>{list}</label>
+                  </div>
+                  <input
+                    type="checkbox"
+                    defaultChecked={props.filterMeta[heading][list]}
+                    id={list}
+                    name={list}
+                    value={list}
+                    onClick={props.filterChange}
+                    data-group={heading}
+                  />
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
+    </React.Fragment>
+  )
 }
 
-export default Filter;
+export default Filter
